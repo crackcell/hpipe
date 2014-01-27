ce77c60aaf3cc7787bcda5bf6c4ba7edd75e04ad
Menglong Tan <tanmenglong@gmail.com>
2014-01-28 01:31:40 +0800

    add run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-##! @description: usage
-##! @version: 1
-##! @author: crackcell <tanmenglong@gmail.com>
-##! @date:   Tue Jan 28 01:07:29 2014
-
-source conf/corgi.conf
-./run.py flow/wordcount.xml
