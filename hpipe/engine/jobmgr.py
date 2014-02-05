3b3a167cdff815b4fcd6b91d084ae33641c6f3e9
Menglong Tan <tanmenglong@gmail.com>
2014-02-05 16:06:55 +0800

    enhance grammer

--- hpipe/engine/jobmgr.py
+++ hpipe/engine/jobmgr.py
@@ -14,6 +14,7 @@ from datetime import datetime, timedelta
 from hpipe.util import setup_logger
 from hpipe.engine.entity import Node, Job
 from hpipe.engine.launcher import Launcher
+from hpipe.engine.grammer import ReferenceResolver
 
 logger = logging.getLogger(__name__)
 setup_logger(logger)
@@ -27,7 +28,7 @@ class JobManager(object):
         self.pattern = re.compile("\$\{(.+?)([-+]*)(\d*)(d*)\}")
 
     def load_conf(self, conf_file):
-        self.__parse_file(self.root, conf_file)
+        self.__parse_file(self.root, conf_file, {})
         logger.debug("node tree:")
         self.__log_tree(self.root)
 
@@ -35,17 +36,17 @@ class JobManager(object):
         logger.info("launching jobs")
         self.launcher.launch(self.root)
 
-    def __parse_file(self, node, conf_file):
+    def __parse_file(self, node, conf_file, local_space):
         xmlroot = ET.parse(conf_file).getroot()
 
         if xmlroot.tag == "flow":
-            self.__parse_flow(node, xmlroot)
+            self.__parse_flow(node, xmlroot, local_space)
         elif xmlroot.tag == "job":
-            self.__parse_job(node, xmlroot)
+            self.__parse_job(node, xmlroot, local_space)
         else:
             raise RuntimeError("invalid xml tag '%s'" % xmlroot.tag)
 
-    def __parse_flow(self, node, xmlroot):
+    def __parse_flow(self, node, xmlroot, local_space):
 
         try:
             node.name = xmlroot.attrib["name"]
@@ -58,14 +59,13 @@ class JobManager(object):
         for child in xmlroot:
             if child.tag != "node":
                 continue
-            node_name, node_res, node_deps = self.__parse_node_info(child)
+            node_name, node_res, node_deps, local_space = self.__parse_node(child)
             # format:
-            #  {node_name : [node_object, deps_dict, depended_by_others]}
-            deps[node_name] = [Node(node_name, node_res), node_deps,
-                               False]
+            #  {node_name : [node_object, deps_dict, depended_by_others, local_space]}
+            deps[node_name] = [Node(node_name, node_res), node_deps, False, local_space]
 
         for name, dep in deps.iteritems():
-            self.__parse_file(dep[0], dep[0].resource)
+            self.__parse_file(dep[0], dep[0].resource, dep[3])
             # parse dependence
             for dep_name in dep[1]:
                 if deps[dep_name] == None:
@@ -83,90 +83,53 @@ class JobManager(object):
 
         return node
 
-    def __parse_node_info(self, xmlroot):
+    def __parse_node(self, xmlroot):
         name = None
         res = None
         deps = {}
+        local_space = {}
         for child in xmlroot:
             if child.tag == "name":
                 name = child.text
             elif child.tag == "resource":
                 res = child.text
-            elif child.tag == "dependencies":
-                for dep in child.text.split(","):
-                    deps[(dep.strip())] = 0
+            elif child.tag == "depend":
+                deps[child.text.strip()] = 0
+            elif child.tag == "property":
+                pname, pvalue = self.__parse_property(child)
+                local_space[pname] = pvalue
             else:
                 raise RuntimeError("invalid property: %s" % child.tag)
 
         if name == None or res == None:
             raise RuntimeError("invalid node, missing resource")
 
-        return name, res, deps
+        return name, res, deps, local_space
 
-    def __parse_job(self, node, xmlroot):
+    def __parse_job(self, node, xmlroot, local_space):
         job = Job()
         resolved = {}
         for child in xmlroot:
             if child.tag != "property":
                 continue
-            name, value = self.__parse_property_info(child)
+            name, value = self.__parse_property(child)
             if name == "hpipe.file":
                 job.files.append(value)
                 continue
             job.properties[name] = value
             resolved[name] = len(re.findall("\$\{(.+?)\}", value)) == 0
 
-        for name, value in job.properties.iteritems():
+        # process grammer
+        ref_resolver = ReferenceResolver()
+        ref_resolver.add_space(os.environ)
+        ref_resolver.add_space(local_space)
+        ref_resolver.add_space(job.properties)
+        ref_resolver.resolve()
 
-            for match in self.pattern.findall(value):
-                var = match[0]
-                if len(match) != 1 and len(match) != 3 and len(match) != 4:
-                    raise RuntimeError("invalid property value: %s" %
-                                       value)
-
-                resolved_value = None;
-
-                if var in job.properties and resolved[var]:
-                    resolved_value = job.properties[var]
-                elif var in os.environ:
-                    resolved_value = os.environ[var]
-                else:
-                    raise RuntimeError("invalid property, "
-                                       "undefined or unresolved "
-                                       "variable: ${%s}" %
-                                       var)
-
-                if match[3] == "":     # normal numeric calculation
-                    if match[1] == "-":
-                        resolved_value = str(int(resolved_value) -
-                                             int(match[2]))
-                    elif match[1] == "+":
-                        resolved_value = str(int(resolved_value) +
-                                             int(match[2]))
-
-                else:  # datetime caculation
-                    orig_time = datetime.strptime(resolved_value, "%Y%M%d")
-                    if match[3] == "d":   # day diff
-                        diff_time = timedelta(days=int(match[2]))
-                    elif match[3] == "w": # week diff
-                        diff_time = timedelta(weeks=int(match[2]))
-                    else:
-                        raise RuntimeError("invalid datettime unit: %s" %
-                                           match[3])
-
-                    if match[1] == "-":
-                        resolved_value = (orig_time -
-                                          diff_time).strftime("%Y%M%d")
-                    elif match[1] == "+":
-                        resolved_value = (orig_time +
-                                          diff_time).strftime("%Y%M%d")
-
-                orig = job.properties[name]
-                job.properties[name] = job.properties[name].replace(
-                    "${" + match[0] + match[1] + match[2] + match[3] + "}",
-                    resolved_value)
-                logger.debug("resolving: %s -> %s",
-                             orig, job.properties[name])
+        for k, v in job.properties.iteritems():
+            orig = job.properties[k]
+            job.properties[k] = ref_resolver.space[k]
+            logger.debug("resolved $%s: %s -> %s", k, orig, job.properties[k])
 
         try:
             job.validate()
@@ -176,7 +139,7 @@ class JobManager(object):
 
         node.job = job
 
-    def __parse_property_info(self, xmlroot):
+    def __parse_property(self, xmlroot):
         name = None
         value = None
         for child in xmlroot:
