859826dde6ca03e83caa321b23e37c025398c609
Menglong Tan <tanmenglong@gmail.com>
2014-02-08 09:59:12 +0800

    mv join related code to separate folder

---
+++ smartflow/python/join_mapper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; indent-tabs-mode: nil -*-
+#
+# Copyright 2014 Menglong TAN <tanmenglong@gmail.com>
+#
+
+class JoinMapper(object):
+
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    pass
