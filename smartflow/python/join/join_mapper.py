44a81b2cef5e940339739e4b08d3c4598bdf3a22
Menglong Tan <tanmenglong@gmail.com>
2014-02-09 23:54:40 +0800

    update demo

--- smartflow/python/join/join_mapper.py
+++ smartflow/python/join/join_mapper.py
@@ -5,11 +5,26 @@
 #
 
 import os
+import sys
 
 class JoinMapper(object):
 
     def __init__(self):
-        pass
+        self.sub_mappers = {}
+        for input_name in os.environ["hpipe_flow_join_inputs"].split(","):
+            input_name = input_name.strip()
+            input_path = os.environ["hpipe_flow_join_%s_input_dir" % input_name]
+            mapper_file = os.environ["hpipe_flow_join_%s_mapper" % input_name]
+            self.sub_mappers[input_path] = mapper_file
+
+    def map(self, line):
+        input_path = os.environ["map_input_file"]
+        if not input_path in self.sub_mappers:
+            raise RuntimeError("invalid input path: %s" % input_path)
+        mapper_file = self.sub_mappers[input_path]
 
 if __name__ == "__main__":
-    pass
+    mapper = JoinMapper()
+    for line in sys.stdin:
+        # remove leading and trailing whitespace
+        line = line.strip()
