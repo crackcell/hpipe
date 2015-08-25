daf16f466a2809645a0b77cbb3fec4cbbddff06c
Menglong TAN <tanmenglong@gmail.com>
2015-08-25 18:33:36 +0800

    add shell exec

---
+++ exec/hadoop.go
@@ -0,0 +1,33 @@
+/***************************************************************
+ *
+ * Copyright (c) 2015, Menglong TAN <tanmenglong@gmail.com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GPL licence
+ *
+ **************************************************************/
+
+/**
+ *
+ *
+ * @file hadoop.go
+ * @author Menglong TAN <tanmenglong@gmail.com>
+ * @date Tue Aug 25 18:28:05 2015
+ *
+ **/
+
+package exec
+
+import (
+//"fmt"
+)
+
+//===================================================================
+// Public APIs
+//===================================================================
+
+type HadoopExec struct{}
+
+//===================================================================
+// Private
+//===================================================================
